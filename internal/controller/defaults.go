4fb53554341eb1aebb3f4217244e51135dc42c6a
Praveen M <m.praveen@ibm.com>
2025-07-17 21:20:06 +0530

    csi: new csi-snapshot-metadata sidecar integration
    KEP-3314 introduces new CSI APIs to identify changed blocks
    between snapshots of CSI volumes. These APIs enable efficient
    and incremental backups by allowing backup applications to
    retrieve only the data that has changed.
    
    To support this feature, Ceph-CSI should include an
    external-snapshot-metadata sidecar in the RBD controller plugin.
    
    This commit adds the new snapshot-metadata sidecar to RBD
    controller plugin if the SnapshotMetadataService CR for the RBD
    driver exists in the cluster.
    
    Signed-off-by: Praveen M <m.praveen@ibm.com>

--- internal/controller/defaults.go
+++ internal/controller/defaults.go
@@ -28,13 +28,14 @@ import (
 )
 
 var imageDefaults = map[string]string{
-	"provisioner": "registry.k8s.io/sig-storage/csi-provisioner:v5.3.0",
-	"attacher":    "registry.k8s.io/sig-storage/csi-attacher:v4.9.0",
-	"resizer":     "registry.k8s.io/sig-storage/csi-resizer:v1.13.2",
-	"snapshotter": "registry.k8s.io/sig-storage/csi-snapshotter:v8.2.0",
-	"registrar":   "registry.k8s.io/sig-storage/csi-node-driver-registrar:v2.14.0",
-	"plugin":      "quay.io/cephcsi/cephcsi:v3.14.1",
-	"addons":      "quay.io/csiaddons/k8s-sidecar:v0.12.0",
+	"provisioner":       "registry.k8s.io/sig-storage/csi-provisioner:v5.3.0",
+	"attacher":          "registry.k8s.io/sig-storage/csi-attacher:v4.9.0",
+	"resizer":           "registry.k8s.io/sig-storage/csi-resizer:v1.13.2",
+	"snapshotter":       "registry.k8s.io/sig-storage/csi-snapshotter:v8.2.0",
+	"registrar":         "registry.k8s.io/sig-storage/csi-node-driver-registrar:v2.14.0",
+	"snapshot-metadata": "registry.k8s.io/sig-storage/csi-snapshot-metadata:v0.1.0",
+	"plugin":            "quay.io/cephcsi/cephcsi:v3.14.1",
+	"addons":            "quay.io/csiaddons/k8s-sidecar:v0.12.0",
 }
 
 const (
